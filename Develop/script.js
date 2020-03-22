186d043f85a6388b40ed4be047c16d23edd47b03
Lisheng <lishengyu1986@gmail.com>
2020-03-22 14:44:40 +0800

    basic button-view ajax query works

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
-    $("#search-btn").on("click", function (event) {
+    $("#button-view").on("click", function (event) {
         event.preventDefault();
 
+        var date = moment().format('L');
+
         var locations = ["Austin", "Chicago", "New York", "Orlando", "San Francisco", "Seattle", "Denver", "Atlanta"];
+
+        var city = event.target.innerHTML;
+        console.log(city);
 
         var apiKey = "b9cd52a37f4e83dc74f86603a4adc81a";
 
         var queryURL =
-          "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
-    
+          "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        
+        // add UV index here  
+        var queryUV =  
+          "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            $("#weather-now").append([
-                $("<h2>", { class: ""})
+            $("#city").text(response.name + "(" + date + ")" + response.weather[0].icon);
+            $("#temp").text(response.main.temp);
+            $("#rh").text(response.main.humidity);
+            $("#ws").text(response.wind.speed);
 
-
-            ])
-
-
-        })  
-    
+        });  
     
     
     
