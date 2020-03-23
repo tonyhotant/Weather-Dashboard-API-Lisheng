030f8190a16ca3cadfac416ff1e33a048df5ec18
Lisheng <lishengyu1986@gmail.com>
2020-03-23 18:08:17 +0800

    major update, all function works expect UVindex

--- Develop/script.js
+++ Develop/script.js
@@ -3,110 +3,110 @@ $(document).ready(function() {
 
   var date = moment().format("L");
 
-  var locations = [
-    "Austin",
-    "Chicago",
-    "New York",
-    "Orlando",
-    "San Francisco",
-    "Seattle",
-    "Denver",
-    "Atlanta"
-  ];
+  var locations = [];
 
+  init();
+
+  $("#button-view").on("click", function(event) {
+    event.preventDefault();
+    var city = event.target.innerHTML;
+    displayData(city);
+  });
+
+  $("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    var city = $("#user-input").val();
+    if (city == "" || locations.includes(city)) {
+      return;
+    }
+
+    var newCity = $("<button>");
+    newCity.addClass("list-group-item list-group-item-action").text(city);
+    $("#button-view").append(newCity);
+    $(".list-group-item")
+      .first()
+      .remove();
+    locations.shift();
+    locations.push(city);
+    localStorage.setItem("history", JSON.stringify(locations));
+
+    displayData(city);
+  });
 
   function init() {
-        //check if localstorage is empty
-        //get data, append to buttons and display
-
-        //renderBtn()
-        //displayDta()
-  }
-
-
-  function renderBtn() {
-        //get city name from user input
-        //append to button list
-        //display the last city data
-
-        //displayData()
-  }
-
-
-    function searchCity() {
-      //get user input
-      //append to button-view
-      //save to localstorage
+    var history = JSON.parse(localStorage.getItem("history"));
+    if (history == null) {
+      history = [
+        "Austin",
+        "Chicago",
+        "New York",
+        "Orlando",
+        "San Francisco",
+        "Seattle",
+        "Denver",
+        "Atlanta"
+      ];
+    } else {
+      locations = history;
+      for (i = 0; i < locations.length; i++) {
+        $("#city-" + i).text(locations[i]);
+      }
     }
-
-
-    $("#search-btn").on("click", function(event) {
-        event.preventDefault();
-        var city = $("#user-input").val();
-
-        displayData(city);
-
-        var newCity = $("<button>");
-        newCity.addClass("list-group-item list-group-item-action");
-
-    })
-
-    
-    $("#button-view").on("click", function(event) {
-      event.preventDefault();
-
-      var city = event.target.innerHTML;
-      displayData(city);
-    });
-
-
+    displayData(locations[0]);
+  }
 
   function displayData(city) {
-      var queryURL =
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=" +
-        apiKey;
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey;
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        var iconCode = response.weather[0].icon;
-        var iconURL =
-          "http://openweathermap.org/img/w/" + iconCode + ".png";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var iconCode = response.weather[0].icon;
+      var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
 
-        $("#city").text(response.name + "(" + date + ")");
-        $("#icon").attr("src", iconURL);
-        $("#temp").text(response.main.temp);
-        $("#rh").text(response.main.humidity);
-        $("#ws").text(response.wind.speed);
+      $("#city").text(response.name + "(" + date + ")");
+      $("#icon").attr("src", iconURL);
+      $("#temp").text(response.main.temp);
+      $("#rh").text(response.main.humidity);
+      $("#ws").text(response.wind.speed);
+    });
 
+    var forecastURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=" +
+      apiKey;
+    $.ajax({
+      url: forecastURL,
+      method: "GET"
+    }).then(function(response) {
+      for (i = 1; i < 6; i++) {
+        var forecastDt = moment()
+          .add(i, "days")
+          .format()
+          .slice(0, 10);
+        var index = response.list
+          .map(function(e) {
+            return e.dt_txt;
+          })
+          .indexOf(forecastDt + " 09:00:00");
+        var iconCode = response.list[index].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
 
-      });
-
-      var forecastURL =
-        "https://api.openweathermap.org/data/2.5/forecast?q=" +
-        city +
-        "&appid=" +
-        apiKey;
-      $.ajax({
-          url:forecastURL,
-          method: "GET"
-      }).then(function(response) {
-        //to be continue
-
-      });
-
-
-    }
-
-
-
-
-
-
-
-
-
+        $("#date-" + i).text(
+          moment()
+            .add(i, "days")
+            .format("L")
+        );
+        $("#icon-" + i).attr("src", iconURL);
+        $("#temp-" + i).text(response.list[index].main.temp);
+        $("#rh-" + i).text(response.list[index].main.humidity);
+      }
+    });
+  }
 });
